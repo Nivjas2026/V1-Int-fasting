97cc86cc14eb60d3d6370f142c34ef684b5b75ea
Nivjas2026 <nivjas2003@gmail.com>
2026-09-24 21:28:51 +0530

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,4 @@
+const C='ft-v2',A=['./','index.html','manifest.json','icon-192.png','icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!=C).map(x=>caches.delete(x))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(C).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
